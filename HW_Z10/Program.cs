9d249d2bb88f8f668299709ad4329ab211f5b2e2
Vitaliy <V.tal@mail.ru>
2022-09-01 21:44:30 +0700

    10

---
+++ HW_Z10/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int number = new Random().Next(100, 1000);
+int firstnumber = number % 100;
+int secondnumber = firstnumber / 10;
+
+Console.WriteLine($"Вторая цифра числа {number} - {secondnumber}");
